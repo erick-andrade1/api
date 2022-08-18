425a04d76011f67cd39a36aafbebc0382b49cc12
erick-andrade1 <erickandradesilva@hotmail.com>
2022-08-17 23:48:38 -0300

    Exemplo de get e post no websocket

--- WEBSOCKET/main.go
+++ WEBSOCKET/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
@@ -13,11 +14,18 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: 1024,
 }
 
+type userExample struct {
+	Username string `json:"username"`
+	Name     string `json:"name"`
+	LastName string `json:"lastName"`
+}
+
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Home page")
 }
 
 func reader(conn *websocket.Conn) {
+	// Exemplo do websocket lendo algo recebido do front:
 	for {
 		messageType, p, err := conn.ReadMessage()
 		if err != nil {
@@ -34,7 +42,35 @@ func reader(conn *websocket.Conn) {
 	}
 }
 
-func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+func sender(conn *websocket.Conn) {
+	// Exemplo do websocket enviando algo para o front:
+	ch := time.Tick(5 * time.Second)
+	for range ch {
+		conn.WriteJSON(userExample{
+			Username: "Basilisk Aeron",
+			Name:     "Erick",
+			LastName: "Veríssimo",
+		})
+	}
+}
+
+func websocketGet(w http.ResponseWriter, r *http.Request) {
+
+	// Permite qualquer conexão com o endpoint websocket
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
+	// "upando" a conexão para websocket
+	websocket, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println("Client connected....")
+
+	go sender(websocket)
+}
+
+func websocketPost(w http.ResponseWriter, r *http.Request) {
 	// Permite qualquer conexão com o endpoint websocket
 	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
 
@@ -51,7 +87,8 @@ func wsEndpoint(w http.ResponseWriter, r *http.Request) {
 
 func setupRoutes() {
 	http.HandleFunc("/", homePage)
-	http.HandleFunc("/ws", wsEndpoint)
+	http.HandleFunc("/ws/post", websocketPost)
+	http.HandleFunc("/ws/get", websocketGet)
 }
 
 func main() {
